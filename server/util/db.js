6c13abe57a37fcf9a14a0fe198aac38d1b78a2a1
Vipul Chaudhary <143116654+vipulbharadwaj@users.noreply.github.com>
2025-07-05 11:27:24 +0530

    Update db.js

--- server/util/db.js
+++ server/util/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-//const uri = "mongodb+srv://thevipulbhardwaj:dnaca6LUpCh51bsu@cluster-chat.qntpvtb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-chat"
+
 const connectDB = async()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI);
